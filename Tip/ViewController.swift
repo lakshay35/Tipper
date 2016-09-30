266d47e4e3bc6cea8fcb50824f7e153740e880ac
Lakshay Sharma <lakshay35@gmail.com>
2016-09-30 15:58:19 -0400

    General Interface Working Now 1. Need to implement error handling

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -10,16 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    var tip = 0
+    var bill = 0
+    var userTyping = false
+    var dotNotPressed = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func updateLabel() {
+        let total: Double = ((Double(tip)/100.00) * Double(bill)) + Double(bill)
+        totalLabel.text = String(total)
     }
 
+    @IBAction func calculateTip(_ sender: UISlider) {
+        tipLabel.text = String(format: "%\(".2")f", round(slider.value * 30)) + "%"
+        tip = Int(slider.value * 30)
+        updateLabel()
+    }
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+        if userTyping {
+            let currentText = billLabel.text
+            billLabel.text = currentText! + sender.currentTitle!
+            let number = String(bill) + sender.currentTitle!
+            bill = Int(number)!
+        } else {
+            billLabel.text = "$" + sender.currentTitle!
+            bill = Int(sender.currentTitle!)!
+        }
+        userTyping = true
+        updateLabel()
+    }
 
+    @IBAction func dotPressed(_ sender: UIButton) {
+        if dotNotPressed && userTyping {
+            let currentText = billLabel.text
+            billLabel.text = currentText! + "."
+        }
+    }
+
+    @IBAction func clearBill(_ sender: UIButton) {
+        billLabel.text = "$0.00"
+        bill = 0
+        userTyping = false
+        updateLabel()
+    }
+    
 }
 
