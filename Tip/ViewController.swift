8738e14dd885d501c8ac58d8a1ffdad5db26caf2
Lakshay Sharma <lakshay35@gmail.com>
2016-10-01 20:04:09 -0400

    Fully functional application. 1. All bugs have been fixed. 2. Need to implement Launch Screen 3. Needs Licensing and Publishing

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -10,60 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Objects in View
     @IBOutlet weak var billLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
     
+    // Functional Variables
     var tip = 0
-    var bill = 0
+    var bill = 0.0
     var userTyping = false
-    var dotNotPressed = false
+    var dotNotPressed = true
+    var decimalValue = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    
+    // Updates Total Label Value
     func updateLabel() {
-        let total: Double = ((Double(tip)/100.00) * Double(bill)) + Double(bill)
-         //let total: Double = ((Double(tip)/100.00) * bill) + bill
-        totalLabel.text = "$" + String(total)
+        let total: Double = ((Double(tip)/100.00) * bill) + bill // Calculates Total
+        let roundedValue = round(total * 100) // Rounds Total Value
+        totalLabel.text = "$" + String(roundedValue / 100) // Updates Total Label
     }
 
+    // Calculates The Tip Decimal Value To Multiply With Bill Value
     @IBAction func calculateTip(_ sender: UISlider) {
         tipLabel.text = String(format: "%\(".2")f", round(slider.value * 30)) + "%"
-        tip = Int(slider.value * 30)
-        updateLabel()
-    }
+        tip = Int(round(slider.value * 30))
+        updateLabel()     }
     
+    // Updates Labels And Variable For Functionality
     @IBAction func numberPressed(_ sender: UIButton) {
+        // If User Has Typed Atleast Once
         if userTyping {
             let currentText = billLabel.text
             billLabel.text = currentText! + sender.currentTitle!
-            let number = String(bill) + sender.currentTitle!
-            bill = Int(number)!
-        } else {
-            billLabel.text = "$" + sender.currentTitle!
-            bill = Int(sender.currentTitle!)!
-            //bill = Double(sender.currentTitle!)!
+           
+            if !dotNotPressed { // Starts Counting Decimal Values
+                let index = billLabel.text?.characters.index(of: ".") // Gets Index Of The Starting Point Of Decimal Value
+                let number = billLabel.text?.substring(from: index!)
+                bill -= decimalValue
+                decimalValue = Double(number!)!
+                bill += decimalValue
+            }
+            else {
+                let number = String(Int(bill)) + sender.currentTitle!
+                bill = Double(number)!
+            }
         }
+        else { // Gets Executed If User Has Not Typed Even Once
+            billLabel.text = "$" + sender.currentTitle!
+            bill = Double(sender.currentTitle!)!
+        }
+        
+        // User Has Typed Atleast Once
         userTyping = true
+        // Live Updating Of Total Label
         updateLabel()
     }
 
+    // Starts Counting Decimal Values
     @IBAction func dotPressed(_ sender: UIButton) {
         if dotNotPressed && userTyping {
             let currentText = billLabel.text
             billLabel.text = currentText! + "."
+            dotNotPressed = false
         }
     }
 
+    // Resets Labels and Functional Variables
     @IBAction func clearBill(_ sender: UIButton) {
         billLabel.text = "$0.00"
         bill = 0
         userTyping = false
+        dotNotPressed = true
+        decimalValue = 0.0
         updateLabel()
     }
     
